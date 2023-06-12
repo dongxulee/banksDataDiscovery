93995a4d9216f0c3a4e9e59a698ebbcaa71731ce
dongxulee <leodongxu@gmail.com>
2023-06-12 17:43:53 -0400

    init

---
+++ helper.py
@@ -0,0 +1,156 @@
+import numpy as np 
+import pandas as pd
+from collections import deque
+import networkx as nx
+from matplotlib import pyplot as plt
+
+df = pd.read_csv('df.csv')
+df["REPDTE"] = pd.to_datetime(df["REPDTE"], format="%Y-%m-%d").dt.date
+
+codes1 = ["ASSET", "CHBAL", "CHBALNI", "CHBALI", "CHCIC", "CHUS","CHNUS", "CHFRB", "SC",
+        "SCUS", "SCMUNI", "SCDOMO", "SCFORD", "SCEQNFT", "SCHA", "FREPO", "LNLSNET", "LNRE", "LNAG",
+        "LNCI", "LNCON", "LNOTCI", "LNCOMRE", "TRADE", "BKPREM", "ORE", "INTAN", "AOA"]
+
+codes2 = ["LIABEQ", "LIAB", "DEP", "DEPDOM", "DEPFOR", "FREPP", 
+         "TRADEL", "OTHBRF", "SUBND", "ALLOTHL", "EQTOT", "EQPP",
+         "EQCS", "EQSUR", "EQUPTOT"]
+
+filenames = ["assetStructure.txt", "liabilityStructure.txt"]
+names = []
+for filename in filenames:
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            name = line
+            names.append(name)
+name2code = dict(zip(names, codes1 + codes2))
+code2name = dict(zip(codes1 + codes2, names))
+
+def banks(year, month, numOfBanks):
+    # The largest 100 banks
+    dates = df["REPDTE"].unique()
+    for day in dates:
+        if day.year == year and day.month == month:
+            largestBanks = df[(df['REPDTE'] == day)].groupby("NAMEFULL").mean().sort_values(by="ASSET", ascending=False).head(numOfBanks).index.values.tolist()
+            dd = df[(df["NAMEFULL"].isin(largestBanks)) & (df['REPDTE'] == day)][["REPDTE", "NAMEFULL"]+list(name2code.values())]
+            dd = dd.groupby("NAMEFULL").mean().sort_values(by="ASSET", ascending=False) 
+    return dd, largestBanks
+
+def bankPlot(bank,type,dd):
+    filename = type + "Structure.txt"
+    def read_tree_structure(filename):
+        def count_indentation(line):
+            count = 0
+            for char in line:
+                if char == ' ':
+                    count += 1
+                elif char == '\t':
+                    count += 4
+                else:
+                    break
+            return count
+
+        tree = {}
+        with open(filename) as f:
+            lines = f.readlines()
+            stack = deque()
+            for line in lines:
+                depth = count_indentation(line) // 4
+                line = line.strip()
+                name = line
+                value = dd.loc[bank][name2code[name]].max()
+
+
+                node = {"name": name, "children": [], "depth": depth, "value": value}
+
+                # Pop elements from the stack until reaching the correct depth
+                while stack and stack[-1]["depth"] >= depth:
+                    stack.pop()
+
+                # If the stack is empty, the node is the root
+                if not stack:
+                    tree = node
+                else:
+                    # Otherwise, append the node to the parent's children
+                    stack[-1]["children"].append(node)
+
+                stack.append(node)
+        return tree
+
+
+    def create_directed_graph(tree):
+        graph = nx.DiGraph()
+        def traverse(node):
+            graph.add_node(node["name"], value=node["value"], depth=node["depth"])
+            for child in node["children"]:
+                graph.add_edge(node["name"], child["name"])
+                traverse(child)
+        traverse(tree)
+        return graph
+
+
+    def format_label(node, value, parent_value):
+        percentage = "" if graph.in_degree(node) == 0 else (value / parent_value)
+        return node, percentage
+
+
+    tree = read_tree_structure(filename)
+    graph = create_directed_graph(tree)
+    node_labels = []
+    for node in graph.nodes:
+        parent_value = graph.nodes[node]["value"] if graph.in_degree(node) == 0 else graph.nodes[list(graph.predecessors(node))[0]]["value"]
+        node_labels.append(format_label(node, graph.nodes[node]["value"], parent_value))
+
+    return node_labels
+
+
+# return banks data frame with the given bank names
+def sdf(year, month, bankNames, numOfBanks = 100):
+    dd, _ = banks(year, month, numOfBanks)
+    d = dd.loc[bankNames]
+    L1 = []
+    L2 = []
+    for bank in bankNames:
+        l1 = bankPlot(bank,'asset', d)
+        l2 = bankPlot(bank,'liability', d)
+        L1.append(np.array(l1)[1:,1].astype(float))
+        L2.append(np.array(l2)[1:,1].astype(float))
+    L1 = np.array(L1)
+    L2 = np.array(L2)
+    
+    X = np.zeros(d[codes1 + codes2].shape)
+    X[:,0] = 0.5
+    X[:,1] = 0.5
+    X[:,2:] = np.concatenate((L1,L2),axis=1)
+    # fill na values in numpy array with 0
+    X = np.nan_to_num(X, copy=False)
+    Code = [codes1[0]] + [codes2[0]] + codes1[1:] + codes2[1:]
+    names = [code2name[code] for code in Code]
+    sdf = pd.DataFrame(X,columns=names)
+    return sdf
+
+
+def pairwiseComparison(years, months, numberOfBanks, xlabel = True):
+    _, banks1 = banks(years[0], months[0], numberOfBanks)
+    _, banks2 = banks(years[1], months[1], numberOfBanks)
+    commonBanks = [bank for bank in banks1 if bank in banks2]
+    
+    index = np.arange(len(commonBanks))
+    
+    d1 = sdf(years[0], months[0], commonBanks)['Held to Maturity (Book Value)']
+    d2 = sdf(years[1], months[1], commonBanks)['Held to Maturity (Book Value)']  
+    if numberOfBanks > 10:
+        diff = d2 - d1
+        colors = ['blue' if val >= 0 else 'red' for val in diff]
+        plt.bar(index, d2 - d1, color = colors)
+    else:
+        width = 0.30
+        plt.bar(index, d1, width, label = "2022")
+        plt.bar(index+width, d2, width, label = "2023")
+
+    plt.xlabel("Banks")
+    if xlabel:
+        plt.xticks(range(len(commonBanks)), commonBanks, rotation='vertical')
+    plt.legend()
+    return d1,d2
---
+++ treeStructureVisualization.py
@@ -0,0 +1,158 @@
+import re
+import numpy as np
+import networkx as nx
+import plotly.graph_objects as go
+from collections import deque
+import pandas as pd 
+
+date = '2022-12-31'
+
+banks = ["BANK OF AMERICA, NATIONAL ASSOCIATION",
+"JPMORGAN CHASE BANK, NATIONAL ASSOCIATION",
+"WELLS FARGO BANK, NATIONAL ASSOCIATION",
+"CITIBANK, NATIONAL ASSOCIATION",
+"SILICON VALLEY BANK",
+"SIGNATURE BANK",
+"FIRST REPUBLIC BANK",
+"GOLDMAN SACHS BANK USA",
+"MORGAN STANLEY BANK, NATIONAL ASSOCIATION",
+"AMERICAN EXPRESS NATIONAL BANK"
+]
+
+
+
+df = pd.read_csv('df.csv')
+
+codes1 = ["ASSET", "CHBAL", "CHBALNI", "CHBALI", "CHCIC", "CHUS","CHNUS", "CHFRB", "SC",
+        "SCUS", "SCMUNI", "SCDOMO", "SCFORD", "SCEQNFT", "SCHA", "FREPO", "LNLSNET", "LNRE", "LNAG",
+        "LNCI", "LNCON", "LNOTCI", "LNCOMRE", "TRADE", "BKPREM", "ORE", "INTAN", "AOA"]
+
+
+codes2 = ["LIABEQ", "LIAB", "DEP", "DEPDOM", "DEPFOR", "FREPP", 
+         "TRADEL", "OTHBRF", "SUBND", "ALLOTHL", "EQTOT", "EQPP",
+         "EQCS", "EQSUR", "EQUPTOT"]
+
+def bankPlot(bank, type):
+    if type == "asset":
+        codes = codes1
+        filename = "assetStructure.txt"
+    else:
+        codes = codes2
+        filename = "liabilityStructure.txt"
+    names = []
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            name = line
+            names.append(name)
+    name2code = dict(zip(names, codes))
+        
+    def read_tree_structure(filename):
+        def count_indentation(line):
+            count = 0
+            for char in line:
+                if char == ' ':
+                    count += 1
+                elif char == '\t':
+                    count += 4
+                else:
+                    break
+            return count
+
+        tree = {}
+        with open(filename) as f:
+            lines = f.readlines()
+            stack = deque()
+            for line in lines:
+                depth = count_indentation(line) // 4
+                line = line.strip()
+                name = line
+                value = df[(df['REPDTE'] == date) & (df["NAMEFULL"] == bank)][name2code[name]].max()
+
+
+                node = {"name": name, "children": [], "depth": depth, "value": value}
+
+                # Pop elements from the stack until reaching the correct depth
+                while stack and stack[-1]["depth"] >= depth:
+                    stack.pop()
+
+                # If the stack is empty, the node is the root
+                if not stack:
+                    tree = node
+                else:
+                    # Otherwise, append the node to the parent's children
+                    stack[-1]["children"].append(node)
+
+                stack.append(node)
+        return tree
+
+
+    def create_directed_graph(tree):
+        graph = nx.DiGraph()
+        def traverse(node):
+            graph.add_node(node["name"], value=node["value"], depth=node["depth"])
+            for child in node["children"]:
+                graph.add_edge(node["name"], child["name"])
+                traverse(child)
+        traverse(tree)
+        return graph
+
+
+    def format_label(node, value, parent_value):
+        percentage = "" if graph.in_degree(node) == 0 else f" ({value / parent_value:.0%})"
+        return f"{node}{percentage}"
+
+
+    def custom_node_positions(graph):
+        node_depths = nx.get_node_attributes(graph, 'depth')
+        positions = {}
+        level_counts = {}
+        depth_count = {}
+        for node, depth in node_depths.items():
+            depth_count[depth] = depth_count.get(depth, 0) + 1
+            level_counts[depth] = level_counts.get(depth, 0) + 1
+            positions[node] = (depth, level_counts[depth])
+        normalized_positions = {
+            node: (
+                max(depth / max(node_depths.values()) - 0.3, 0), 
+                y / depth_count[depth]
+            ) for node, (depth, y) in positions.items()
+        }
+
+        return normalized_positions
+
+    tree = read_tree_structure(filename)
+    graph = create_directed_graph(tree)
+    node_positions = custom_node_positions(graph)
+    node_positions
+
+    node_labels = list(graph.nodes)
+    link_sources = [node_labels.index(link[0]) for link in graph.edges]
+    link_targets = [node_labels.index(link[1]) for link in graph.edges]
+    link_values = [graph.nodes[link[1]]["value"] for link in graph.edges]
+
+    node_labels = []
+    for node in graph.nodes:
+        parent_value = graph.nodes[node]["value"] if graph.in_degree(node) == 0 else graph.nodes[list(graph.predecessors(node))[0]]["value"]
+        node_labels.append(format_label(node, graph.nodes[node]["value"], parent_value))
+
+    sankey_nodes = dict(
+        pad=15,
+        thickness=20,
+        line=dict(color="black", width=0.5),
+        label=node_labels,
+        x=[pos[0] for pos in node_positions.values()],
+        y=[pos[1] for pos in node_positions.values()]
+    )
+
+    sankey_fig = go.Figure(data=[go.Sankey(
+        node=sankey_nodes,
+        link=dict(source=link_sources, target=link_targets, value=link_values)
+    )])
+
+    sankey_fig.update_layout(title_text="Balance Sheet Sankey Diagram: " + bank +" at "+ date, font_size=10, height=800, width=1000)
+    sankey_fig.show()
+
+for bank in banks:
+    bankPlot(bank, "asset")
